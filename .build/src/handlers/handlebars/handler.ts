f9f88640010d6ad8539e53afdbe627cb532b0e1a
drunyan1 <43184854+drunyan1@users.noreply.github.com>
2022-01-27 14:46:53 -0600

    Delete .build directory

--- .build/src/handlers/handlebars/handler.ts
+++
@@ -1,6 +0,0 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { Riposte, RiposteResponse } from '@nbm/riposte';
-
-export const hello = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  return Riposte.ok('Hello!');
-};
